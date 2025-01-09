9aa6a7c85af9ce0cf7f44e343c4a4104f467b68b
risenfromashes <ashrafurkhan37@gmail.com>
2025-01-09 16:57:00 +0600

    perform preprocessing in action

--- preprocess.py
+++ preprocess.py
@@ -2,8 +2,9 @@
 # and write to index.md
 
 import re
+import os
 
-def preprocess():
+def preprocess(input_file="blog.md", output_file="index.md"):
 
     def replacer(m):
         content = m.group(1)
@@ -12,12 +13,17 @@ def preprocess():
         content = re.sub(r'\\}', r'\\\\}', content)
         return "\\\\(" + content + "\\\\)"
 
-    with open("blog.md", "r") as f:
+    with open(input_file, "r") as f:
         data = f.read()
         data = re.sub(r'(?<!\$)\$([^\$]+)\$(?!\$)', replacer, data)
 
-    with open("index.md", "w") as f:
+    with open(output_file, "w") as f:
         f.write(data)
 
 if __name__ == "__main__":
-    preprocess()
+    markdown_files = [f for f in os.listdir(".") if f.endswith(".md")]
+    for f in markdown_files:
+        preprocess(f, f)
+        if f == "blog.md":
+            preprocess(f, "index.md")
+    print("Preprocessing done!")
